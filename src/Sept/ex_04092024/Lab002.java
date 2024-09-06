0ad0adcaaaeef7adb8796ba9b05901ee707b4ec3
Ashit <ashotoshx17@gmail.com>
2024-09-06 08:25:09 +0530

    New code

---
+++ src/Sept/ex_04092024/Lab002.java
@@ -0,0 +1,10 @@
+package Sept.ex_04092024;
+
+public class Lab002 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println(2+2);
+        System.out.println(2-2);
+        System.out.println(2*2);
+    }
+}
