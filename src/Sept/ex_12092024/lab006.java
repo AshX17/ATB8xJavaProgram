bfcdb88c4438a39db0c08b3e72b82af7038c2d63
Ashit <ashotoshx17@gmail.com>
2024-09-16 18:07:21 +0530

    New code

---
+++ src/Sept/ex_12092024/lab006.java
@@ -0,0 +1,17 @@
+package Sept.ex_12092024;
+
+public class lab006 {
+    public static void main(String[] args) {
+        boolean  Ashitosh_123 = true;
+        boolean Kajal$321 = false;
+        System.out.println(Ashitosh_123);
+        System.out.println(Kajal$321);
+        byte m = 100;
+        System.out.println(m);
+        short s = 127;
+        System.out.println(s);
+        byte o = 100;
+        System.out.println(o);
+    }
+}
+
