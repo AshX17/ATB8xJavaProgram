17db2ffd3a8d1c93217c9ff06136883dcff3a315
Ashit <ashotoshx17@gmail.com>
2024-09-22 14:20:29 +0530

    all files

---
+++ src/Sept/ex_13092024/Lab0012.java
@@ -0,0 +1,26 @@
+package Sept.ex_13092024;
+
+public class Lab0012 {
+    public static void main(String[] args) {
+
+        int s = 10;
+        long u = 200l;
+
+        System.out.println(s);
+        System.out.println();
+        System.out.printf("%d * %d = %d",s,1,s*1);
+        System.out.println();
+        System.out.printf("%d * %d = %d",s,2,s*2);
+        System.out.println();
+        System.out.printf("%d * %d = %d",s,3,s*3);
+        System.out.println();
+        System.out.println(s * u);
+        int a = 10;
+        int b = 20;
+        String first_name = "Ashitosh";
+        String last_name = "Kadam";
+
+        System.out.println(first_name +last_name + a + b);
+        System.out.println(first_name +'\n'+ last_name+(a+b));
+    }
+}
---
+++ src/Sept/ex_13092024/Lab0013_assignment.java
@@ -0,0 +1,36 @@
+package Sept.ex_13092024;
+
+public class Lab0013_assignment {
+    // 1 Question answer
+
+    //  = is assignment operator
+    // == is comparison operator
+    // example
+    public static void main(String[] args) {
+        System.out.println("A" == "B");
+        int a = 10;
+        System.out.println(a);
+
+        // 2 Question answer
+        byte b = 10;
+        long c = 10l;
+        // for b - 1 Bytes
+        // for c - 8 Bytes
+
+        // 3 question answer
+        // int is the data type
+
+        // 4 Question answer
+        // short s = 10;
+        // char c = 'A'; // ASIIC value = 65;
+
+        short s = 10;
+        char f = 'A';
+        int ss = s+f;
+
+        System.out.println(ss);
+        // Answer = 75;
+    }
+
+
+}
